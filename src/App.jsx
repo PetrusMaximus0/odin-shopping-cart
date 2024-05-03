54b6c48607e0e1bd0594c74e3854e7a4a1738b01
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-03 15:38:51 +0200

    style: Changed the layout colours

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
 	};
 
 	return (
-		<div className="font-sans text-slate-200 grid gap-4 min-h-screen grid-rows-[auto_1fr_auto]">
-			<header className="bg-teal-950 shadow-lg shadow-teal-950">
+		<div className="font-sans text-teal-950 grid gap-4 min-h-screen grid-rows-[auto_1fr_auto]">
+			<header className="bg-teal-200 shadow-lg shadow-teal-200">
 				<div className="container mx-auto p-8 mb-2 flex flex-col md:flex-row items-center  gap-4 text-center justify-between ">
-					<Link to="/" className="text-2xl italic hover:text-orange-300">
+					<Link to="/" className="text-2xl italic hover:text-orange-400">
 						THE IMPULSE SHOPPER
 					</Link>
 					<Nav cartItemCount={totalItemsInCart()} />
@@ -25,9 +25,9 @@ function App() {
 			<main className="container mx-auto">
 				<Outlet context={[cartItems, setCartItems]} />
 			</main>
-			<footer className="bg-teal-950 shadow-teal-950 shadow-[0px_-5px_10px] text-center py-8 text-lg">
+			<footer className="bg-teal-200 shadow-teal-200 shadow-[0px_-5px_10px] text-center py-8 text-lg">
 				<div className="container mx-auto">
-					<span className="">Copyright 2024 Pedro Silva </span>
+					<span className="text-xl">Copyright 2024 Pedro Silva </span>
 					<a target="_blank" href="https://github.com/PetrusMaximus0">
 						<i className="devicon-github-original"></i>
 					</a>
