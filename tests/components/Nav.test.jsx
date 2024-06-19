942e5755a652cd5ec25c2e59b72ec1a8627ed214
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-19 21:41:24 +0200

    test-wip: Add testing file for the Nav Component

---
+++ tests/components/Nav.test.jsx
@@ -0,0 +1,6 @@
+import Nav from '../../src/components/Nav';
+import { describe, it } from 'vitest';
+
+describe('Nav component', () => {
+	it.todo('shows the correct number of items in the cart');
+});
