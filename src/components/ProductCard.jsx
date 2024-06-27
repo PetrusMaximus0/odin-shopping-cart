348e33945c7ffa4cae61cf08591e462e9b01b6c6
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-27 19:37:23 +0200

    Revert "Revert "refactor: Remove all code related to handling items in cart state. Pass the product data to the AddToCart component.""
    This reverts commit 369be1fe8aa4ef6a930c8e37797d4bede1e329b3.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,27 +1,8 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AddToCartBtn from './AddToCartBtn';
-import { useOutletContext } from 'react-router-dom';
 
 const ProductCard = ({ data }) => {
-	// Access context for adding products to the cart.
-	const [cartItems, setCartItems] = useOutletContext();
-
-	//
-	const handleAddToCart = (number) => {
-		// Check if ID is IN the cartItems
-		const newCartItems = [...cartItems];
-		const index = newCartItems.findIndex((item) => data.id === item.id);
-		if (index !== -1) {
-			//The product is in the cart, add quantity.
-			newCartItems[index].quantity += number;
-			//
-			setCartItems(newCartItems);
-		} else {
-			setCartItems([...cartItems, { ...data, quantity: number }]);
-		}
-	};
-
 	return (
 		<li className="grid gap-4 p-8 bg-white border border-black rounded-2xl text-slate-800 content-between justify-center grid-rows-[150px_repeat(3,auto)]">
 			<figure>
@@ -35,7 +16,7 @@ const ProductCard = ({ data }) => {
 				<Link to={`/products/${data.id}`}> Product {data.title} </Link>
 			</h1>
 			<p className="text-xl font-bold"> {data.price} €</p>
-			<AddToCartBtn handleFormSubmit={handleAddToCart} />
+			<AddToCartBtn data={data} />
 		</li>
 	);
 };
