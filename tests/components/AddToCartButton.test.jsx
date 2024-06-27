149f53a75ca3fd4bf61cd5bd64160ba917814178
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-27 19:40:39 +0200

    tests: Correct test to use context provider, test adding items to the cart with the button.

--- tests/components/AddToCartButton.test.jsx
+++ tests/components/AddToCartButton.test.jsx
@@ -1,18 +1,28 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
-
 import AddToCartBtn from '../../src/components/AddToCartBtn';
 import userEvent from '@testing-library/user-event';
+import CartContext from '../../src/contexts/CartContext';
 
 describe('AddToCartButton Component', () => {
 	const buttonText = 'click';
 
+	const BtnComponentWrapper = (
+		data = {},
+		cartItems = [],
+		setCartItems = () => {}
+	) => {
+		return (
+			<CartContext.Provider value={{ cartItems, setCartItems }}>
+				<AddToCartBtn data={data} btnText={buttonText} />
+			</CartContext.Provider>
+		);
+	};
+
 	describe('Inputing numbers...', () => {
 		it('clicking the plus and minus buttons changes the value on the input. The value does not go below one', async () => {
 			//
-			render(
-				<AddToCartBtn handleFormSubmit={() => {}} btnText={buttonText} />
-			);
+			render(BtnComponentWrapper());
 
 			// Get input field
 			const inputField = screen.getByRole('spinbutton');
@@ -41,9 +51,7 @@ describe('AddToCartButton Component', () => {
 
 		it('Manually inputing a number works correctly', async () => {
 			//
-			render(
-				<AddToCartBtn handleFormSubmit={() => {}} btnText={buttonText} />
-			);
+			render(BtnComponentWrapper());
 
 			//
 			const inputField = screen.getByRole('spinbutton');
@@ -59,56 +67,91 @@ describe('AddToCartButton Component', () => {
 
 			// Entering a non number character makes the form invalid.
 			await user.clear(inputField);
-			await user.type(inputField, 'a');
+			await user.type(inputField, 'c');
 			expect(inputField).toBeInvalid();
 
 			//
 		});
 	});
 
-	describe('Submitting the form', () => {
-		it('The happy path submits correctly, the button text changes on submit', async () => {
-			//
-			const submissionCallback = vi.fn();
-			//
-			render(
-				<AddToCartBtn
-					handleFormSubmit={submissionCallback}
-					btnText={buttonText}
-				/>
-			);
+	describe('Submitting the form...', () => {
+		it('the button text changes on submit and the item is added to cart', async () => {
+			const data = { id: 1 };
+			let cartItems = [{ id: 2, quantity: 1 }];
+			const setCartItems = (newItems) => {
+				cartItems = [...newItems];
+			};
 
 			//
-			const inputField = screen.getByRole('spinbutton');
-			expect(inputField.value).toBe('1');
+			render(BtnComponentWrapper(data, cartItems, setCartItems));
 
-			//
+			// cartItems has only one item
+			expect(cartItems.length === 1).toBeTruthy();
+
+			// Add the item to the cart
 			const submitButton = screen.getByText(/click/);
-
-			//
 			const user = userEvent.setup();
 			await user.click(submitButton);
+
 			await waitFor(async () => {
 				expect(submitButton).toHaveTextContent('Item Added');
 			});
-			expect(submissionCallback).toBeCalled();
+
+			await waitFor(
+				async () => {
+					expect(submitButton).toHaveTextContent(buttonText);
+				},
+				{ timeout: 4000 }
+			);
+
+			// The item was added to cart items
+			expect(
+				cartItems.findIndex((item) => item.id === data.id) !== -1
+			).toBeTruthy();
+
+			// There should now be two items in the cart
+			expect(cartItems.length === 2).toBeTruthy();
 		});
 
-		it('Attempting to submit an invalid form does nothing.', async () => {
-			//
-			const submissionCallback = vi.fn();
+		it('adds correctly to the quantity of an item already in the cart', async () => {
+			const data = { id: 1 };
+			let cartItems = [
+				{ id: 1, quantity: 1 },
+				{ id: 2, quantity: 5 },
+			];
+			const setCartItems = (newItems) => {
+				cartItems = [...newItems];
+			};
 
 			//
-			render(
-				<AddToCartBtn
-					handleFormSubmit={submissionCallback}
-					btnText={buttonText}
-				/>
-			);
+			render(BtnComponentWrapper(data, cartItems, setCartItems));
+
+			// Add an item to the cart
+			const user = userEvent.setup();
+			const submitButton = screen.getByText(/click/);
+			await user.click(submitButton);
+
+			// The cart should have the same number of unique items as the start
+			expect(cartItems.length === 2).toBeTruthy();
+
+			// The added item should have a quantity of 2
+			const index = cartItems.findIndex((item) => item.id === data.id);
+			expect(index !== -1).toBeTruthy();
+			expect(cartItems[index].quantity === 2).toBeTruthy();
+		});
+
+		it('does not submit an invalid form.', async () => {
+			const data = { id: 1123213 };
+			let cartItems = [];
+			const setCartItems = (newItems) => {
+				cartItems = newItems;
+			};
+
+			//
+			render(BtnComponentWrapper(data, cartItems, setCartItems));
 
 			//
 			const inputField = screen.getByRole('spinbutton');
-			expect(inputField.value).toBe('1');
 
 			//
 			const user = userEvent.setup();
@@ -122,8 +165,8 @@ describe('AddToCartButton Component', () => {
 			const submitButton = screen.getByText(/click/);
 			await user.click(submitButton);
 
-			//
-			expect(submissionCallback).not.toBeCalled();
+			// Nothing was added to cartItems.
+			expect(cartItems.length === 0).toBeTruthy();
 		});
 	});
 });
