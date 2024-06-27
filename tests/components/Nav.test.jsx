6844fda96b80dee4740cdc81537cec1773289916
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-06-27 19:41:25 +0200

    tests: correct the test to use the context provider

--- tests/components/Nav.test.jsx
+++ tests/components/Nav.test.jsx
@@ -1,40 +1,45 @@
 import { describe, it } from 'vitest';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import Nav from '../../src/components/Nav';
 import { render, screen } from '@testing-library/react';
 import { expect } from 'vitest';
 
+import CartContext from '../../src/contexts/CartContext';
+import Nav from '../../src/components/Nav';
+
 describe('Nav component', () => {
-	it('presents only the text for the cart icon when no items are in the cart', () => {
-		const router = createMemoryRouter([
+	const getRouter = (items) =>
+		createMemoryRouter([
 			{
 				path: '/',
-				element: <Nav />,
+				element: (
+					<CartContext.Provider value={{ cartItems: items }}>
+						<Nav />
+					</CartContext.Provider>
+				),
 			},
 		]);
-		render(<RouterProvider router={router} />);
-		expect(screen.queryByText(/Cart/)).toBeInTheDocument();
+
+	it('presents only the text for the cart icon when no items are in the cart', () => {
+		const cartItems = [];
+
+		render(<RouterProvider router={getRouter(cartItems)} />);
+
+		expect(screen.queryByText('Cart')).toBeInTheDocument();
 	});
 
-	it('presents the correct number of items in the cart when the number is a positive integer', () => {
-		const routerItems = createMemoryRouter([
-			{
-				path: '/',
-				element: <Nav cartItemCount={10} />,
-			},
-		]);
-		render(<RouterProvider router={routerItems} />);
+	it('presents the correct number of items for the cart icon if there is a single item type', () => {
+		const cartItems = [{ quantity: 10 }];
+
+		render(<RouterProvider router={getRouter(cartItems)} />);
+
 		expect(screen.queryByText('Cart (10)')).toBeInTheDocument();
 	});
 
-	it('presents only the text when it receives errouneously something other than a positive integer', () => {
-		const routerItems = createMemoryRouter([
-			{
-				path: '/',
-				element: <Nav cartItemCount={'a'} />,
-			},
-		]);
-		render(<RouterProvider router={routerItems} />);
-		expect(screen.queryByText('Cart')).toBeInTheDocument();
+	it('presents the correct number of items for the cart icon, if there are different item types', () => {
+		const cartItems = [{ quantity: 5 }, { quantity: 3 }, { quantity: 2 }];
+
+		render(<RouterProvider router={getRouter(cartItems)} />);
+
+		expect(screen.queryByText('Cart (10)')).toBeInTheDocument();
 	});
 });
