a061f7ca232293d48ccd7857d64d6865496c5b0d
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-12-16 14:26:59 +0100

    fix: Set a fixed image size for the hero image.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,11 +14,10 @@ const Home = () => {
 				Buy
 			</Link>
 			<figure className="w-full">
-				<img  className={"mx-auto w-48 h-auto"} src="/home.svg" alt="IMAGE" />
+				<img  className={"mx-auto w-48 h-48"} src="/home.svg" alt="IMAGE" />
 				<legend className="text-center font-thin text-xs">
-					Image by{' '}
 					<a href="https://www.freepik.com/free-vector/flat-hand-drawn-people-shopping-sale-illustration_12151005.htm#query=shopping%20cartoon&position=8&from_view=keyword&track=ais&uuid=2e9ad00f-53a2-4387-bbdd-2c2599db3615">
-						Freepik
+						Image by Freepik
 					</a>
 				</legend>
 			</figure>
