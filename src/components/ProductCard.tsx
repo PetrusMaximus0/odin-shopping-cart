14470de3f1381b873766d9df7a9599fb639fa25a
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-12-16 15:24:36 +0100

    refactor: Adjust the component structure. Set fixed sizes for the image.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,16 +6,16 @@ import AddToCartBtn from './AddToCartBtn';
 const ProductCard = ({ data } : { data: IProduct }) => {
 	return (
 		<li className="grid gap-4 p-8 bg-white border border-black rounded-2xl text-slate-800 content-between justify-center grid-rows-[150px_repeat(3,auto)]">
-			<figure>
+			<figure className={"h-36 w-36 mx-auto"}>
 				<img
-					className="object-contain h-full w-full"
+					className="object-contain h-full w-auto mx-auto"
 					src={data.image}
 					alt="PRODUCT IMAGE"
 				/>
-			</figure>
-			<h1 className="max-w-full text-lg whitespace-nowrap overflow-hidden text-ellipsis">
-				<Link to={`/products/${data.id}`}> Product {data.title} </Link>
-			</h1>
+			</figure>				
+			<Link className="max-w-full text-lg whitespace-nowrap overflow-hidden text-ellipsis" to={`/products/${data.id}`}> 
+				Product {data.title} 
+			</Link>
 			<p className="text-xl font-bold"> {data.price} €</p>
 			<AddToCartBtn btnText={'Add to Cart'} data = { data } />
 		</li>
